bfae1cab29b61cc36db71c4e69af06e9667eb97b
Delgorithm <delgorithm@gmail.com>
2023-07-24 20:31:27 +0200

    added new components

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import Header from '../components/Header/Header.jsx'
+import Header from './components/Header/Header.jsx'
+import Hero from './components/Hero/Hero.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <Header />
+    <Hero />
   </React.StrictMode>,
 )
