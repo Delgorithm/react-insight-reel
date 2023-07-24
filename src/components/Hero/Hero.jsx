c02d5709df2e6df185fefedfa8750eaaaadb7cd9
Delgorithm <delgorithm@gmail.com>
2023-07-24 20:47:02 +0200

    Hero finished

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import './Hero.css';
+import Button from '../Button/Button';
+
 
 const Hero = () => {
   return (
     <section className="hero">
         <div className="left-part">
-            
+            <p>Real People, <br></br>Real Insights</p>
+            <p>Get our qualified UX experts to analyze your <br></br>session recordings and tell you what you should fix. <br></br>Get direct insights to fuel your growth.</p>
+            <Button />
         </div>
+
         <div className="right-part">
-                <img src="./img/img001.jpg" alt="" />
+            <img src="./img/img001.png" alt=""/>
         </div>
     </section>
   )
