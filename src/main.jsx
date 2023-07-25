27cc77e743d821cadd7101662076e61ecca0ea86
Delgorithm <delgorithm@gmail.com>
2023-07-25 16:57:02 +0200

    new component added

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import App from './App.jsx'
 import './index.css'
 import Header from './components/Header/Header.jsx'
 import Hero from './components/Hero/Hero.jsx'
+import Section from './components/Sections/Section.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <Header />
     <Hero />
+    <Section />
   </React.StrictMode>,
 )
