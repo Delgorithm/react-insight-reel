b93a94c0b50e85c56aa63a15eda22dbf898f3671
Delgorithm <delgorithm@gmail.com>
2023-07-26 21:37:04 +0200

    scroll behavior on link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,17 @@ import React from 'react';
 import Button from '../Button/Button';
 import './Header.css'
 
+
+
 const Header = () => {
+    const handleLinkClick = ((event, targetId) => {
+        event.preventDefault();
+        const targetElement = document.getElementById(targetId);
+        if(targetElement) {
+            targetElement.scrollIntoView({ behavior: 'smooth'});
+        }
+    });
+
   return (
     <div>
         <section className="head">
@@ -12,10 +22,10 @@ const Header = () => {
             <div className="navigation">
                 <ul>
                     <li>
-                        <a href="#">About</a>
-                        <a href="#">How it works</a>
-                        <a href="#">Pricing</a>
-                        <a href="#">FAQs</a>
+                        <a href="#about" onClick={(e) => handleLinkClick(e, 'about')}>About</a>
+                        <a href="#how-it-works" onClick={(e) => handleLinkClick(e, 'how-it-works')}>How it works</a>
+                        <a href="#pricing" onClick={(e) => handleLinkClick(e, 'pricing')}>Pricing</a>
+                        <a href="#faqs" onClick={(e) => handleLinkClick(e, 'faqs')}>FAQs</a>
                     </li>
                 </ul>
             </div>
